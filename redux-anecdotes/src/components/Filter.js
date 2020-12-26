5e0fd9c84fbf68c18eb9cb5e425169033471d67b
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-26 12:13:53 +0200

    Add Filter component and use it in App

---
+++ redux-anecdotes/src/components/Filter.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { setFilter } from '../reducers/filterReducer'
+
+const Filter = () => {
+  const dispatch = useDispatch()
+
+  const handleChange = (event) => {
+    event.preventDefault()
+    const { value } = event.target
+    dispatch(setFilter(value))
+  }
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
