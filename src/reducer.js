d5159fa6a133f4e7ee2fc80b1e60de3202fda7db
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-26 10:46:09 +0200

    Implement other actions and tests
    This marks my answer for exercise 6.1.

--- src/reducer.js
+++ src/reducer.js
@@ -6,17 +6,19 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
+  const newState = { ...state }
   switch (action.type) {
     case 'GOOD':
-      const newState = { ...state }
       newState.good = newState.good + 1
       return newState
     case 'OK':
-      return state
+      newState.ok = newState.ok + 1
+      return newState
     case 'BAD':
-      return state
+      newState.bad = newState.bad + 1
+      return newState
     case 'ZERO':
-      return state
+      return initialState
     default: return state
   }
   
